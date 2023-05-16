bcd6ba9fd9c786f142c01b8d01bd1ecec221c017
Giovanna Rocha <silvarochagiovanna@gmail.com>
2023-05-16 18:58:02 -0300

    att

--- cadastrar.js
+++ cadastrar.js
@@ -2,17 +2,17 @@ var area = document.getElementById("area");
 // var nomeCadastro = "Giovanna Rocha";
 var botao = document.getElementById("acessar");
 
-var nomes= new Array(5);
-nomes [0] = "Giovanna";
-nomes [1] = "Ellen";
-nomes [2] = "Mirian";
-nomes [3] = "Luan";
-nomes [4] = "Eric";
+var nomes = new Array(5);
+nomes[0] = "Giovanna";
+nomes[1] = "Ellen";
+nomes[2] = "Mirian";
+nomes[3] = "Luan";
+nomes[4] = "Eric";
 
 function entrar() {
     var nome = prompt("Digite seu nome");
 
-    if (nome === nomes[0] || nome === nomes[1] || nome === nomes[2] || nome === nomes[3] || nome === nomes[4] ) {
+    if (nome === nomes[0] || nome === nomes[1] || nome === nomes[2] || nome === nomes[3] || nome === nomes[4]) {
         area.innerHTML = "Bem-vindo " + nome + "!  <br>";
         botao.setAttribute("hidden", "true");
     }
